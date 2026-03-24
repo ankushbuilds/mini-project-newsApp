9431f0fc0253452993ec4352404579072039ea29
Ankush Thakur <ankushthakur5981@gmail.com>
2026-03-25 01:00:01 +0530

    no change

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,94 +3,92 @@ import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 
-
 export class News extends Component {
   static defaultProps = {
     pageSize: 9
   }
- static propTypes = {
-  pageSize: PropTypes.number,
-  category: PropTypes.string
-}
-  
-        constructor(){  
-        super();
-        this.state = {
-            articles: [],
-            loading : false,
-            page: 1
 
-        }
-        
+  static propTypes = {
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+
+  constructor(){  
+    super();
+    this.state = {
+      articles: [],      // 👈 always start with empty array
+      loading : false,
+      page: 1,
+      totalResults: 0
     }
-      async componentDidMount(){ // lifecycle method that runs once , immediately !!
-      let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&apiKey=048d21c7c8ef41689023ea3bf94e774e&page=1&pageSize=${this.props.pageSize}`;
-       this.setState({loading : true})
+  }
+
+  async componentDidMount(){ 
+    this.fetchNews(this.state.page);
+  }
+
+  fetchNews = async(pageNumber) => {
+    this.setState({loading: true});
+    let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&apiKey=048d21c7c8ef41689023ea3bf94e774e&page=${pageNumber}&pageSize=${this.props.pageSize}`;
+    try {
       let data = await fetch(url);
       let parsedData = await data.json();
-      console.log(parsedData);
-      this.setState({articles: parsedData.articles, 
-        totalResults: parsedData.totalResults,
-      loading: false});
+      // 👇 fallback if API fails
+      this.setState({
+        articles: parsedData.articles || [],
+        totalResults: parsedData.totalResults || 0,
+        loading: false,
+        page: pageNumber
+      });
+    } catch (error) {
+      console.error("Failed to fetch news:", error);
+      this.setState({
+        articles: [],
+        loading: false
+      });
     }
-
-    handleBackClick = async() => {
-           let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&apiKey=048d21c7c8ef41689023ea3bf94e774e&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading : true})
-           let data = await fetch(url);
-      let parsedData = await data.json();
-      console.log(parsedData);
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false
-    })
-      
-    };
-   handleNextClick = async() => {
-    if(this.state.page + 1 <= Math.ceil(this.state.totalResults/this.props.pageSize)){  // no. of pages
-       let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&apiKey=048d21c7c8ef41689023ea3bf94e774e&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-      this.setState({loading : true})
-       let data = await fetch(url);
-      let parsedData = await data.json();
-    
-    this.setState({
-      page: this.state.page + 1,
-      articles: parsedData.articles,
-      loading: false
-    })
   }
-    };
 
+  handleBackClick = () => {
+    this.fetchNews(this.state.page - 1);
+  }
 
-  
+  handleNextClick = () => {
+    if(this.state.page + 1 <= Math.ceil(this.state.totalResults / this.props.pageSize)){
+      this.fetchNews(this.state.page + 1);
+    }
+  }
 
   render() {
-    console.log("render");
     return (
       <div className='container my-3'>
-        <h1 className="text-center" style={{margin: '30px 0px'}}>Latest News  </h1>
+        <h1 className="text-center" style={{margin: '30px 0px'}}>Latest News</h1>
         {this.state.loading && <Spinner/>}
-    
-      <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-        <NewsItem  title ={element.title}  description={element.description ? element.description.slice(0,80) : ""} ImageUrl = {element.urlToImage} newsUrl = {element.url}/>
-        </div>
-         } ) }
-       
-      </div>
-      <div className="container d-flex justify-content-between">
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handleBackClick}>&larr; Back</button>
-      <button disabled ={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
 
+        <div className="row">
+          {!this.state.loading && this.state.articles && this.state.articles.length > 0 ? (
+            this.state.articles.map((element) => (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title}
+                  description={element.description ? element.description.slice(0, 80) : ""}
+                  ImageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            ))
+          ) : (
+            !this.state.loading && <p className="text-center">No news available</p>
+          )}
+        </div>
+
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handleBackClick}>&larr; Back</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
       </div>
-      </div>
-     
     )
   }
 }
 
-
 export default News
-
